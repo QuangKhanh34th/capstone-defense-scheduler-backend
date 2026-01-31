94124e7bad8c03135c549e17c869c80cc8fbdb7a
extend-dcode <duongcongdanh369@gmail.com>
2026-01-31 20:54:25 +0700

    feat: Add feature view list project with sort and filter

--- capstone-defense-scheduler-backend/src/main/java/com/capstone/scheduler/repository/ProjectRepository.java
+++ capstone-defense-scheduler-backend/src/main/java/com/capstone/scheduler/repository/ProjectRepository.java
@@ -2,11 +2,12 @@ package com.capstone.scheduler.repository;
 
 import com.capstone.scheduler.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
+public interface ProjectRepository extends JpaRepository<Project, Integer>, JpaSpecificationExecutor<Project> {
     // Tìm đề tài theo học kỳ
     List<Project> findBySemester_SemesterId(Integer semesterId);
 }
