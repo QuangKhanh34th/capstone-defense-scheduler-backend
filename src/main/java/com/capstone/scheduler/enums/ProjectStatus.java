9513122e3c1792314ff5948622ff42774afbe41f
CongMinh0604 <MinhPCSE180071@fpt.edu.vn>
2026-03-12 23:04:12 +0700

    add enum comment

--- src/main/java/com/capstone/scheduler/enums/ProjectStatus.java
+++ src/main/java/com/capstone/scheduler/enums/ProjectStatus.java
@@ -2,7 +2,7 @@ package com.capstone.scheduler.enums;
 
 public enum ProjectStatus {
     PENDING,    // Đang thực hiện / Chờ bảo vệ
-    COMPLETED,  // Đã bảo vệ và Đạt (Pass)
-    //FAILED,     // Bảo vệ trượt
-    DELETED     // Xóa mềm
+    COMPLETED,  // Chuyển sang khi RoundProjectStatus là PASSED hoặc SemesterStatus là FINISHED
+    //FAILED,     // Bảo vệ trượt (bỏ qua không dùng)
+    DELETED     // Xóa mềm, do user đặt
 }
--- src/main/java/com/capstone/scheduler/enums/RoundProjectStatus.java
+++ src/main/java/com/capstone/scheduler/enums/RoundProjectStatus.java
@@ -2,6 +2,6 @@ package com.capstone.scheduler.enums;
 
 public enum RoundProjectStatus {
     IN_PROGRESS, // Chưa có kết quả (Đang xếp lịch hoặc Đang bảo vệ)
-    PASSED,      // Đậu
-    FAILED       // Trượt
+    PASSED,      // Đậu, do user set
+    FAILED       // Trượt, do user set
 }
